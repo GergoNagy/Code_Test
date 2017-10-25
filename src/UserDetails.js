0b5c9c96d45364a0ec091782a00dc1aad20ab22f
Gergo Nagy <guzmop@gmail.com>
2017-10-25 21:43:23 +0100

    seach after user account

--- src/UserDetails.js
+++ src/UserDetails.js
@@ -4,6 +4,12 @@ const UserDetails = ({user, accounts}) => {
     if(!user) return null
     let account = []
 
+    const searchAccount = Object.keys(accounts).map(key => {
+        if (key === user.userAcc) {
+            account.push(accounts[key].apps)
+        }
+    })
+
     return (
         <div className='user-details'>
             <h2>{user.userName}</h2>
