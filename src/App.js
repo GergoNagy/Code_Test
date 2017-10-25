2a7ab3ed2731e07559fb1b508cbcf10c274dc4ce
Gergo Nagy <guzmop@gmail.com>
2017-10-25 21:14:30 +0100

    connecting to firebase and store the data in state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as firebase from 'firebase'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      accounts: [],
+      users: [],
+      selectedUser: null
+    }
+  }
+
+  componentDidMount(){
+    const rootRef = firebase.database().ref()
+    rootRef.on('value', snap => {this.setState(snap.val())})
+  }
+  
   render() {
     return (
       <div className="App">
